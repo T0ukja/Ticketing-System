aa14a5688f90776d34f363ccddfa3607d7ca57ef
Janne <48018732+ok1231234@users.noreply.github.com>
2022-11-05 00:39:31 +0200

    Raw email get conversion added

--- BlazorApp1/Data/Emails.cs
+++ BlazorApp1/Data/Emails.cs
@@ -8,7 +8,77 @@ namespace BlazorApp1.Data
 
     public class Emails
     {
-      
+
+          public List<ConversationNode>? getconversion(string messageid)
+        //  public Collection<Item>? getconversion(string messageid)
+        {
+         
+            string[] credentials = File.ReadAllText(@".\credentials.txt").Split(new[] { ' ' }); ;
+            string emailaddress = credentials[0];
+            string password = credentials[1];
+            string emailurl = credentials[2];
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            service.Credentials = new WebCredentials(emailaddress, password);
+            service.TraceEnabled = true;
+            service.TraceFlags = TraceFlags.All;
+            service.Url = new Uri(emailurl);
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            //Collection<Item> items = new Collection<Item>();
+            List<ConversationNode> items = new List<ConversationNode>();
+            try
+            {
+                // Find an item in a conversation. Find the first item.
+                // FindItemsResults<Item> results = service.FindItems(WellKnownFolderName.Inbox, UserId.Equals(messageid), new ItemView(1));
+                /*  FindItems(WellKnownFolderName.Inbox,
+                                                                 new ItemView());*/
+                EmailMessage email = EmailMessage.Bind(service, new ItemId(messageid));
+                // Get the conversation identifier of an item. 
+                ConversationId convId = email.ConversationId;
+                //      Console.WriteLine(convId, "Convid tässä");
+                // Specify the properties that will be 
+                // returned for the items in the conversation.
+                PropertySet properties = new PropertySet(BasePropertySet.IdOnly,
+                                                          ItemSchema.Subject,
+                                                          ItemSchema.DateTimeReceived, ItemSchema.TextBody, ItemSchema.IsFromMe);
+
+                // Identify the folders to ignore.
+                Collection<FolderId> foldersToIgnore = new Collection<FolderId>() { WellKnownFolderName.DeletedItems, WellKnownFolderName.Drafts };
+
+                // Request conversation items. This results in a call to the service.         
+                ConversationResponse response = service.GetConversationItems(convId,
+                                                                               properties,
+                                                                               null,
+                                                                               foldersToIgnore,
+                                                                               ConversationSortOrder.TreeOrderDescending);
+
+                // Get the synchronization state of the conversation.
+ 
+
+                foreach (ConversationNode node in response.ConversationNodes)
+                {
+                   // Console.WriteLine("Parent conversation index: " + node.Items[0].Subject);
+                    items.Add(node);
+                    /*foreach (Item item in node.Items)
+                    {
+                        Console.WriteLine("   Item ID: " + item.Id.UniqueId);
+                        Console.WriteLine("   Subject: " + item.Subject);
+                        Console.WriteLine("   Sender: " + item.IsFromMe);
+                        Console.WriteLine("   Received: " + item.DateTimeReceived);
+                        Console.WriteLine("   Body: " + item.TextBody.Text);
+                        items.Add(item);
+                    }*/
+                }
+            }
+            // This exception may occur if there is an error with the service.
+            catch (ServiceResponseException srException)
+            {
+                Console.WriteLine(srException);
+            }
+            return items;
+        }
+
+//*****************************************************************************************
         public List<EmailMessage>? Getemails()
         {
             string[] credentials = File.ReadAllText(@".\credentials.txt").Split(new[] { ' ' }); ;
@@ -18,7 +88,7 @@ namespace BlazorApp1.Data
           
 
 
-            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
             service.Credentials = new WebCredentials(emailaddress,password);
             service.TraceEnabled = true;
             System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
