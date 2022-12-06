f5f66173e313d085cb41ba84ae72ad5b1dddc38d
Janne <48018732+ok1231234@users.noreply.github.com>
2022-12-06 22:10:46 +0200

    one modal is now everypage to show emails and minor changes in code, mainly cleaning

--- BlazorApp1/Data/Emails.cs
+++ BlazorApp1/Data/Emails.cs
@@ -7,14 +7,7 @@ using MongoDB.Driver;
 using BlazorApp1.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.AspNetCore.Components.Authorization;
-using BlazorApp1.Authentication;
-using System.Security.Cryptography.Xml;
-using System.Xml.Linq;
-using static MongoDB.Driver.WriteConcern;
-using System.Runtime.CompilerServices;
-using Blazorise;
-using Microsoft.VisualBasic;
+
 
 namespace BlazorApp1.Data
 {
@@ -65,7 +58,6 @@ namespace BlazorApp1.Data
 			mongoClient = new MongoClient(
 			settingsmodel.Value.ConnectionString);
 
-
 			mongoDatabase_newemails = mongoClient.GetDatabase(
 				settingsmodel_newemails.Value.DatabaseName);
 
@@ -78,13 +70,13 @@ namespace BlazorApp1.Data
 			mongoDatabase = mongoClient.GetDatabase(
 				settingsmodel.Value.DatabaseName);
 
-
+			// Reading ends.
 	//*****************************************************
-
+		// Assinging collections with 
 			emailCollection = mongoDatabase.GetCollection<Datamodel>(
 				settingsmodel.Value.CollectionName);
 
-			commentsCollection = mongoDatabase_comments.GetCollection<Comments>(
+			commentsCollection = mongoDatabase.GetCollection<Comments>(
 				settingsmodel_comments.Value.CollectionName);
 
 			newEmailsCollection = mongoDatabase.GetCollection<NewEmails>(
@@ -115,7 +107,7 @@ namespace BlazorApp1.Data
 		}
 
 	
-
+		// For changing color code in list
 		public async void UpdateColorStatus()
 		{
 			List<Datamodel> listofemails = new List<Datamodel>();
@@ -124,11 +116,18 @@ namespace BlazorApp1.Data
 
 			foreach (Datamodel moro in listofemails)
 			{
+				// Create datetime variable from Datamodel object.
 				DateTime ticketdate = moro.datetimereceived;
+
+				// Comparing datatimes to get hours.
 				var result = (DateTime.UtcNow - moro.datetimereceived).TotalHours;
+
+				// Options/filter to update values.
 				var options = new UpdateOptions { IsUpsert = true };
 				var filter = Builders<Datamodel>.Filter.Eq("_id", moro.id);
 
+
+				// if else to set color code.
 				if (result > 24 && result < 72)
 				{
 
@@ -155,28 +154,81 @@ namespace BlazorApp1.Data
 
 
 		}
+
+
+		// After reading unreaded mails, this is called to delete documents from collection by id.
 		public async void DeleteUnreadedMails(string messageconversationid)
 		{
 
-
+			// If there is multiple unreaded mails in collection
 			if ((newEmailsCollection.CountDocuments(x => x.EmailConversationId == messageconversationid)) > 1)
 			{
 				await newEmailsCollection.DeleteManyAsync(Builders<NewEmails>.Filter.Lt("EmailConversationId", messageconversationid));
 
 			}
 			else
+				
 			{
+				// One unreaded mail only. 
 				newEmailsCollection.DeleteOne(x => x.EmailConversationId == messageconversationid);
 
 			}
 
 		}
+
+		// This is called timer to get unreaded mails
 		public async Task<List<NewEmails>> GetUnreadedMails()
 		{
-
+			// Gets all of the unreaded mails to list and returns it.
 			return newEmailsCollection.Find(_ => true).ToList();
 		}
 
+
+		
+		
+
+		// Get's comments.
+		public async Task<List<Comments>> GetComments(string messageid)
+		{
+			// Get's comment collection by messageid and returns it.
+			List<Comments> comments = commentsCollection.Find(x => x.message_id.Equals(messageid)).ToList();
+			return comments;
+		}
+
+
+		// Function to send comments.
+		public async void SendComment(string messageid,string message, string sender)
+		{
+			// Creates Comments variable and assings values to it.
+			Comments comments = new Comments();
+			comments.message_id = messageid;
+			comments.message = message;
+			comments.time = DateTime.Now;
+			comments.sender = sender;
+
+			// Insert's document to collection.
+			commentsCollection.InsertOneAsync(comments);
+
+		}
+
+
+		// Gets Tickets that are assigned for spesific user.
+		public async Task<List<Datamodel>> GetMessagesDBInProgressUserAsync(string name)
+		{
+
+			// Getting list of tickets by user name.
+			List<Datamodel> listOfJobsInProgress = emailCollection.Find(x => x.handler.Equals(name)).ToList();
+			return listOfJobsInProgress;
+
+
+		}
+
+		// Email functions
+		//********************************************************************************'
+		// Function to send email (forwarded)
+
+
+		// Gets conversation which is showed in ui.
 		public async Task<ConversationResponse> GetConversion(string messageId)
 		{
 			// List declaration for items in conversation.
@@ -199,7 +251,7 @@ namespace BlazorApp1.Data
 				// Properties for what to return for each email.
 				PropertySet properties = new PropertySet(BasePropertySet.IdOnly,
 														  ItemSchema.Subject,
-														  ItemSchema.DateTimeReceived, ItemSchema.TextBody, ItemSchema.IsFromMe, ItemSchema.DisplayTo, ItemSchema.DisplayCc, ItemSchema.MimeContent, EmailMessageSchema.From,EmailMessageSchema.ToRecipients,EmailMessageSchema.CcRecipients);
+														  ItemSchema.DateTimeReceived, ItemSchema.TextBody, ItemSchema.IsFromMe, ItemSchema.DisplayTo, ItemSchema.DisplayCc, ItemSchema.MimeContent, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.CcRecipients);
 
 				// Identify the folders to ignore.
 				Collection<FolderId> foldersToIgnore = new Collection<FolderId>() { WellKnownFolderName.DeletedItems, WellKnownFolderName.Drafts };
@@ -207,17 +259,7 @@ namespace BlazorApp1.Data
 				// Request conversation items. This results in a call to the service.         
 				response = service.GetConversationItems(convId, properties, null, foldersToIgnore, ConversationSortOrder.TreeOrderDescending);
 
-				/* await System.Threading.Tasks.Task.Run(() =>
-						 {
-					 // Get the synchronization state of the conversation.
-					 Parallel.ForEach(response.ConversationNodes, node =>
-					 {
-						 items.Add(node);
 
-					 });
-				 });
-
-						  */
 			}
 			// This exception may occur if there is an error with the service.
 			catch (ServiceResponseException srException)
@@ -231,58 +273,28 @@ namespace BlazorApp1.Data
 
 
 		}
-		
-		public async Task<List<Comments>> GetComments(string messageid)
-		{
-			List<Comments> comments = commentsCollection.Find(x => x.message_id.Equals(messageid)).ToList();
-			return comments;
-		}
 
-		public async void SendComment(string messageid,string message, string sender)
-		{
-
-			Comments comments = new Comments();
-			comments.message_id = messageid;
-			comments.message = message;
-			comments.time = DateTime.Now;
-			comments.sender = sender;
-			commentsCollection.InsertOneAsync(comments);
-
-		}
-
-		public async Task<List<Datamodel>> GetMessagesDBInProgressUserAsync(string name)
-		{
-
-			//var filter = Builders<Datamodel>.Filter.Ne("handler", name);
-			List<Datamodel> listOfJobsInProgress = emailCollection.Find(x => x.handler.Equals(name)).ToList();
-			return listOfJobsInProgress;
-
-
-		}
-
-
-
-
-
-
-
-
-
-		
 		public async void SendForwardEmail(ItemId id, string text, List<string> receiversList, List<string> CCList)
 		{
 			FindItemsResults<Item> findResults;
 			service.Url = new Uri(emailurl);
 			int offSet = 0;
 			// How many to get at function call (loads this many messages at one time).
-			int pageSize = 20;
+			int pageSize = 5;
 	
+			// needed to use EWS.
 			PropertySet propSet = new PropertySet(BasePropertySet.IdOnly, ItemSchema.LastModifiedTime);
 			EmailMessage message = EmailMessage.Bind(service, id, propSet);
 
+			// Creates response by forward.
 			ResponseMessage response = message.CreateForward();
+
+			// Sets text for forward message.
 			response.BodyPrefix = text;
 
+
+
+			// Looping lists of CC/to recipients.
 			foreach (string s in CCList)
 			{
 				response.CcRecipients.Add(s);
@@ -300,16 +312,9 @@ namespace BlazorApp1.Data
 
 
 
-
-
-//********************************************************************************************************
-
-
-
+		// Response function.
 		public async void SendMail(ItemId id, string text, List<string> receiversList, List<string> CCList)
 		{
-			_logger.LogInformation("Sendmail funktiossa");
-
 			FindItemsResults<Item> findResults;
 			service.Url = new Uri(emailurl);
 			int offSet = 0;
@@ -335,101 +340,19 @@ namespace BlazorApp1.Data
 
 			response.SendAndSaveCopy();
 
-		//	}
-			/*
-             *       ResponseMessage response = item.CreateReply(false);
-                 response.BodyPrefix = text;
-                 response.SendAndSaveCopy();
-            */
-
-		}
-		public async Task<List<Datamodel>> getHistory()
-		{
-
-			//var filter = Builders<Datamodel>.Filter.Ne("handler", name);
-			List<Datamodel> ListHistory = historyCollection.Find(x => x.status.Equals("1")).ToList();
-			return ListHistory;
-
-
 		}
 
-		public async Task<Datamodel> GetTicketStatus(string messageid)
-		{
-			Datamodel email = new Datamodel();
-			email = emailCollection.Find(x => x.message_id.Equals(messageid)).FirstOrDefault();
-			return email;
-		}
-		public async Task<List<Datamodel>> GetMessagesDBInProgressAsync()
-		{
-
-			var filter = Builders<Datamodel>.Filter.Ne("handler", "");
-			List<Datamodel> listOfJobsInProgress = emailCollection.Find(filter).ToList();
-			return listOfJobsInProgress;
-
-
-		}
-		// Function used to get messages from database
-		public async Task<List<Datamodel>> GetMessagesDbAsync()
-		{
-
-			List<Datamodel> listOfEmails = emailCollection.Find(x => x.handler.Equals("")).ToList();
-			return listOfEmails;
-
-		}
-
-		public async void AssingTicket(string name, string messageid)
-		{
-
-			// Try catch block to assign user object id to spesific ticket.
-			try
-			{
-
-				var update = Builders<Datamodel>.Update.Set("handler", name).Set("status", "3");
-
-				var filter = Builders<Datamodel>.Filter.Eq("message_id", messageid);
-				Datamodel ticket = emailCollection.Find(x => x.message_id == messageid).FirstOrDefault();
-				var options = new UpdateOptions { IsUpsert = true };
-				emailCollection.UpdateOne(filter, update, options);
-				// _logger.LogInformation(ticket.subject.ToString());
-			}
-			catch (InvalidCastException e)
-			{
-				_logger.LogInformation(e.ToString());
-			}
-
-		}
-		public void SetTicketStatus(string _value, string solvetext, string messageid)
-		{
-
-			Datamodel ticket = emailCollection.Find(x => x.message_id == messageid).FirstOrDefault();
-			if (_value == "1")
-			{
-				ticket.status = _value;
-				ticket.solution = solvetext;
-				historyCollection.InsertOneAsync(ticket);
-				emailCollection.DeleteOne(x => x.message_id == messageid);
-			}
-			else
-			{
-				var update = Builders<Datamodel>.Update.Set("status", _value).Set("solution", solvetext);
-				var filter = Builders<Datamodel>.Filter.Eq("message_id", messageid);
-				var options = new UpdateOptions { IsUpsert = true };
-				emailCollection.UpdateOne(filter, update, options);
-			}
 
 
 
-			//emailCollection.InsertOneAsync(emailModel);
-
-		}
 		// Function used to get unreaded emails and insert them to database.
 		public void GetEmails()
 		{
 			EmailMessage helpdeskemail = new EmailMessage(service);
 
 
-		// Variable declaration.
-		FindItemsResults<Item> findResults;
+			// Variable declaration.
+			FindItemsResults<Item> findResults;
 			// offSet
 			int offSet = 0;
 			// How many to get at function call (loads this many messages at one time).
@@ -518,33 +441,134 @@ namespace BlazorApp1.Data
 
 
 				}
-				else {
-				if ((message.IsRead == false)) //if the current message is unread
+				else
 				{
-					message.Load();
-					message.IsRead = true;
-					
-					if ( (newEmailsCollection.CountDocuments(x=>x.EmailConversationId == message.ConversationId))>= 1)
+					if ((message.IsRead == false)) //if the current message is unread
 					{
+						message.Load();
+						message.IsRead = true;
+
+						if ((newEmailsCollection.CountDocuments(x => x.EmailConversationId == message.ConversationId)) >= 1)
+						{
+
+						}
+						else
+						{
+							moro.EmailConversationId = message.ConversationId.ToString();
+
+							newEmailsCollection.InsertOneAsync(moro);
+						}
+
+						message.Update(ConflictResolutionMode.AlwaysOverwrite);
 
 					}
-					else
-					{
-						moro.EmailConversationId = message.ConversationId.ToString();
-
-						newEmailsCollection.InsertOneAsync(moro);
-					}
-
-					message.Update(ConflictResolutionMode.AlwaysOverwrite);
 
 				}
 
-                }
-
-            }
+			}
 
 
 		}
+
+
+
+		// End of email functions
+		//***********************************************************************
+		public async Task<List<Datamodel>> getHistory()
+		{
+
+			//var filter = Builders<Datamodel>.Filter.Ne("handler", name);
+			List<Datamodel> ListHistory = historyCollection.Find(x => x.status.Equals("1")).ToList();
+			return ListHistory;
+
+
+		}
+
+		public async Task<Datamodel> GetTicketStatus(string messageid)
+		{
+			Datamodel email = new Datamodel();
+			email = emailCollection.Find(x => x.message_id.Equals(messageid)).FirstOrDefault();
+			return email;
+		}
+		public async Task<List<Datamodel>> GetMessagesDBInProgressAsync()
+		{
+
+			var filter = Builders<Datamodel>.Filter.Ne("handler", "");
+			List<Datamodel> listOfJobsInProgress = emailCollection.Find(filter).ToList();
+			return listOfJobsInProgress;
+
+
+		}
+		// Function used to get messages from database
+		public async Task<List<Datamodel>> GetMessagesDbAsync()
+		{
+
+			List<Datamodel> listOfEmails = emailCollection.Find(x => x.handler.Equals("")).ToList();
+			return listOfEmails;
+
+		}
+		public async void ChangeTicketCollection(string messageid, string status)
+		{
+			try
+			{
+				Datamodel ticket = emailCollection.Find(x => x.message_id == messageid).FirstOrDefault();
+
+				ticket.status = status;
+				ticket.solution = "";
+				emailCollection.InsertOneAsync(ticket);
+				historyCollection.DeleteOne(x => x.message_id == messageid);
+			}
+			catch (InvalidCastException e)
+			{
+				_logger.LogInformation(e.ToString());
+			}
+		}
+		public async void AssingTicket(string name, string messageid)
+		{
+
+			// Try catch block to assign user object id to spesific ticket.
+			try
+			{
+
+				var update = Builders<Datamodel>.Update.Set("handler", name).Set("status", "3");
+
+				var filter = Builders<Datamodel>.Filter.Eq("message_id", messageid);
+				Datamodel ticket = emailCollection.Find(x => x.message_id == messageid).FirstOrDefault();
+				var options = new UpdateOptions { IsUpsert = true };
+				emailCollection.UpdateOne(filter, update, options);
+				// _logger.LogInformation(ticket.subject.ToString());
+			}
+			catch (InvalidCastException e)
+			{
+				_logger.LogInformation(e.ToString());
+			}
+
+		}
+		public void SetTicketStatus(string _value, string solvetext, string messageid)
+		{
+
+			Datamodel ticket = emailCollection.Find(x => x.message_id == messageid).FirstOrDefault();
+			if (_value == "1")
+			{
+				ticket.status = _value;
+				ticket.solution = solvetext;
+				historyCollection.InsertOneAsync(ticket);
+				emailCollection.DeleteOne(x => x.message_id == messageid);
+			}
+			else
+			{
+				var update = Builders<Datamodel>.Update.Set("status", _value).Set("solution", solvetext);
+				var filter = Builders<Datamodel>.Filter.Eq("message_id", messageid);
+				var options = new UpdateOptions { IsUpsert = true };
+				emailCollection.UpdateOne(filter, update, options);
+			}
+
+
+
+			//emailCollection.InsertOneAsync(emailModel);
+
+		}
+
 
 
 		// Uselessa at the moment.
